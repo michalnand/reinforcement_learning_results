f9ee3c6ee028f74a877e1de5685d7500676e9976
michalnand <michal.nand@gmail.com>
2024-01-03 13:44:57 +0100

    private eye

---
+++ atari_hard/private_eye/batch.sh
@@ -0,0 +1,18 @@
+python3 training_3.py &
+sleep 60
+python3 training_4.py &
+sleep 60
+python3 training_5.py &
+sleep 60
+
+wait
+
+
+python3 training_6.py &
+sleep 60
+python3 training_7.py &
+sleep 60
+python3 training_8.py &
+sleep 60
+
+wait
